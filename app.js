cb833707edb54e003e7cbd1a62ec2d5624e7d3d2
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-28 04:47:41 +0800

    測試

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
-const mongoose = require('mongoose');
 const mongodbLib = require('./mongodbLib.js');
 const mongodbUri = process.env.MONGODB_URI;
 
 mongodbLib.connect(mongodbUri);
-mongodbLib.insert('diary-dbs', {
-    user: String,
-    content: String,
-    time: { type: Date, default: Date.now }
-}, {
-        user: '陳',
-        content: '今天天氣真好'
-    });
-
-
-// var schema = mongodbLib.getSchema({
-//     user: String,
-//     content: String,
-//     time: { type: Date, default: Date.now }
-// });
-
-// var diary = mongoose.model('diary-dbs', schema);
-// var diaryNode = new diary({
-//     user: '高宇哲',
-//     content: '今天天氣真好'
-// });
-
-// diaryNode.save((err) => { if (err) { console.log(err); } });
-
+mongodbLib.insert('diary-dbs', {user: String, content: String, time: { type: Date, default: Date.now } }, { user: '陳', content: '今天天氣真好' });
