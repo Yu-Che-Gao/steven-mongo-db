3f703dcb98aa581cbb781d77357cff3ba2c7e772
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-28 04:49:19 +0800

    測資更改

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const mongodbLib = require('./mongodbLib.js');
 const mongodbUri = process.env.MONGODB_URI;
 
 mongodbLib.connect(mongodbUri);
-mongodbLib.insert('diary-dbs', {user: String, content: String, time: { type: Date, default: Date.now } }, { user: '陳', content: '今天天氣真好' });
+mongodbLib.insert('diary-dbs', {user: String, content: String, time: { type: Date, default: Date.now } }, { user: '高', content: '今天天氣真不好' });
