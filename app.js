e06678dcea7919060845b1aa6017cd7c5855da41
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-28 20:42:44 +0800

    更正

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ app.get('/', (req, res) => {
     promise.then(
         (result) => { return result; }, (error) => { console.log(error); }
     ).then((value) => {
-        return mongodbLib.close(db);
-    }).then((value) => {
+        mongodbLib.close(db);
         return res.send(value);
     })
 })
