c43eff248be6f966d67a5fb5c0d39d2034b130d1
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-29 05:23:34 +0800

    變更順序性任務

--- diary-dbs.js
+++ diary-dbs.js
@@ -5,22 +5,21 @@ var schemaInstance, model;
 
 function connect(mongodbUri, collection, schema) {
     mongoose.Promise = global.Promise;
-    schemaInstance = new mongoose.Schema({
-        user: String,
-        content: String,
-        time: {
-            type: Date,
-            default: Date.now
-        }
-    });
-
-    model = mongoose.model(collection, schemaInstance);
-
     return mongoose.connect(mongodbUri, (err, res) => {
         if (err) {
             console.log('Error connection to: ' + mongodbUri + '. error: ' + err);
         } else {
             console.log('Succeeded connection to: ' + mongodbUri);
+            schemaInstance = new mongoose.Schema({
+                user: String,
+                content: String,
+                time: {
+                    type: Date,
+                    default: Date.now
+                }
+            });
+
+            model = mongoose.model(collection, schemaInstance);
         }
     })
 }
