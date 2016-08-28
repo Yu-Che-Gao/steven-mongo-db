69c9bec1e20c6845e252cb15429466e6e38428bb
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-28 18:11:54 +0800

    修正

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-require('express-mongoose');
+
 const app = express();
 const mongodbLib = require('./mongodbLib.js');
 const promiseLib = require('./promiseLib.js');
 const mongodbUri = process.env.MONGODB_URI;
 const port = process.env.PORT || 5000;
 
+app.use(require('express-mongoose')());
 mongodbLib.connect(mongodbUri);
 // mongodbLib.insert('diary-dbs', {user: String, content: String, time: { type: Date, default: Date.now } }, { user: '高', content: '今天天氣真不好' });
 
@@ -14,8 +15,10 @@ mongodbLib.connect(mongodbUri);
 app.get('/', (req, res) => {
     let promise = mongodbLib.select('diary-dbs', { user: String, content: String, time: { type: Date, default: Date.now } }, { user: '高' })
     promise.then(
-        (result) => { res.send(result); }, (error) => { console.log(error); }
+        (result) => { return result; }, (error) => { console.log(error); }
     )
+
+    res.send({ promise: promise });
 })
 
 app.listen(port, () => { console.log('listening on port ' + port) });
