e9a6dab3d530203e3c8f4dcdc46f67b3a0aad381
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-28 19:51:51 +0800

    更改順序性任務

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.get('/', (req, res) => {
         (result) => { return result; }, (error) => { console.log(error); }
     ).then((value) => {
         mongodbLib.close(db);
+        return value;
+    }).then((value) => {
         return res.send(value);
     })
 })
