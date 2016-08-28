b2c9cccbdd3a5ea8541834687ac305f1483096e6
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-28 18:45:05 +0800

    微調

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 // var promise = promiseLib.getNewPromise(['diary-dbs', { user: String, content: String, time: { type: Date, default: Date.now } }, { user: '高' }]);
 
 app.get('/', (req, res) => {
-    let connection=mongodbLib.connect(mongodbUri);
+    let connection = mongodbLib.connect(mongodbUri);
     let promise = mongodbLib.select('diary-dbs', { user: String, content: String, time: { type: Date, default: Date.now } }, { user: '高' })
     promise.then(
         (result) => { return result; }, (error) => { console.log(error); }
