386bb035ba228719251ed90f5d28744e1ded75d7
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-29 09:40:00 +0800

    測試

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose=require('mongoose');
-const diaryDbs = require('./diary-dbs.js');
+const mapMongo = require('./map-mongo.js');
 const port = process.env.PORT || 5000;
 const mongodbUri = process.env.MONGODB_URI;
 
 mongoose.connect(mongodbUri);
-app.get('/', (req, res) => {
-    diaryDbs.find({ user: '高宇哲' }).exec().then((result) => {
-        return res.send(result);
-    });
-});
+app.get('/', mapMongo.listDiary);
 app.listen(port, () => { console.log('listening on port ' + port) });
