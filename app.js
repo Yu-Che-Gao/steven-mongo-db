6c04a64eb741fa3717f2aea1496c2f0e5bb3bfa9
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-29 09:37:14 +0800

    更正

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose=require('mongoose');
 const diaryDbs = require('./diary-dbs.js');
 const port = process.env.PORT || 5000;
+const mongodbUri = process.env.MONGODB_URI;
 
+mongoose.connect(mongodbUri);
 app.get('/', (req, res) => {
     diaryDbs.find({ user: '高宇哲' }).exec().then((result) => {
         return res.send(result);
--- diary-dbs.js
+++ diary-dbs.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const mongodbUri = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
