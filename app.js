c59b6cd9bfccb1e7c285932d02b0d0bab7976bc5
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-29 10:24:19 +0800

    引用body-parser

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser=require('body-parser');
 const mongoose = require('mongoose');
 const mapMongo = require('./map-mongo.js');
 const port = process.env.PORT || 5000;
 const mongodbUri = process.env.MONGODB_URI;
 
+app.use(bodyParser.urlencoded({extended: true}))
 mongoose.connect(mongodbUri);
 app.get('/', (req, res) => { res.send('you have no right to access this page.') });
 app.post('/list-diary', mapMongo.listDiary);
--- map-mongo.js
+++ map-mongo.js
@@ -1,5 +1,4 @@
 const diaryDbs = require('./diary-dbs.js');
-const bodyParser=require('body-parser');
 function listDiary(req, res) {
     diaryDbs.find({ user: req.body.user }).exec().then((result) => { return res.send(result); });
 }
