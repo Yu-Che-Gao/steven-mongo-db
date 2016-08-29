ffee379d98fccb19b9d22844087291670829a95a
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-29 09:26:34 +0800

    更新

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const mongodbLib = require('./map-mongo.js');
+const mapMongo = require('./map-mongo.js');
 const port = process.env.PORT || 5000;
 
-app.get('/', mongodbLib.listDiary);
+app.get('/', mapMongo.listDiary);
 app.listen(port, () => { console.log('listening on port ' + port) });
