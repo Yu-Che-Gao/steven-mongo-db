04bcb9466d65a2cba73e0c37b0b81c75327aa6fd
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-29 09:33:07 +0800

    修改

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const mapMongo = require('./map-mongo.js');
+const diaryDbs = require('./diary-dbs.js');
 const port = process.env.PORT || 5000;
 
-app.get('/', mapMongo.listDiary);
+app.get('/', (req, res) => {
+    diaryDbs.find({ user: '高宇哲' }).exec().then((result) => {
+        res.send(result);
+    });
+});
 app.listen(port, () => { console.log('listening on port ' + port) });
