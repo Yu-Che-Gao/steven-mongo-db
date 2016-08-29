d7b15aaa7c998612d91e2fb06b3c143339023003
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-29 09:23:41 +0800

    微調

--- diary-dbs.js
+++ diary-dbs.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const mongodbUri = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
-const schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const diaryDbs = new Schema({
