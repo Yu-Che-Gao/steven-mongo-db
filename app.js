1003c22c2b7fd107a3d25b806fc66a30b5d1c5c7
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-29 10:11:17 +0800

    微調

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const mapMongo = require('./map-mongo.js');
 const port = process.env.PORT || 5000;
 const mongodbUri = process.env.MONGODB_URI;
 
 mongoose.connect(mongodbUri);
-app.get('/list-diary', mapMongo.listDiary);
+app.get('/', (req, res) => { res.send('you have no right to access this page.') });
+app.post('/list-diary', mapMongo.listDiary);
 app.listen(port, () => { console.log('listening on port ' + port) });
