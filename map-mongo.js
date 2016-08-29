dc9a826e5b3712f60a7ae9fe0b4612638ce5ea1b
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-29 09:38:36 +0800

    測試

--- map-mongo.js
+++ map-mongo.js
@@ -1,7 +1,7 @@
 const diaryDbs = require('./diary-dbs.js');
 
 function listDiary(req, res) {
-    diaryDbs.find({ user: '高宇哲' }).exec().then((result) => { res.send(result); });
+    diaryDbs.find({ user: '高' }).exec().then((result) => { res.send(result); });
 }
 
 exports.listDiary = listDiary;
