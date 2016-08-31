209dfb11ee9d80c2b3038aa6fca8afed558a23d2
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-31 10:59:33 +0800

    修正引入錯誤

--- controllers/ws-echo.js
+++ controllers/ws-echo.js
@@ -1,6 +1,6 @@
 const wsEchoModel = require('../models/ws-echo.js');
 const express = require('express');
-const router = express();
+const router = express.Router();
 
 router.ws('/echo', wsEchoModel.echoSameMessage);
 module.exports = router;
