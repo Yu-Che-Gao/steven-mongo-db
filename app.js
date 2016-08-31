6ce25f7af1332f9744b78297c34b7ab4ad837a92
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-31 10:21:52 +0800

    引入模組

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-const bodyParser=require('body-parser');
+const expressWs = require('express-ws')(app);
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 const mongodbUri = process.env.MONGODB_URI;
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 mongoose.connect(mongodbUri);
 app.use(require('./controllers'))
 app.listen(port, () => {
-    console.log('listening on port ' + port) 
+    console.log('listening on port ' + port)
 });
