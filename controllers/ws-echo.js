81c24379397693399630de6eb18fcb86e68be097
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-31 13:34:37 +0800

    加入express-ws

--- controllers/ws-echo.js
+++ controllers/ws-echo.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const app = express();
+const expressWs = require('express-ws')(app);
 const router = express.Router();
 const wsEchoModel = require('../models/ws-echo.js');
 
