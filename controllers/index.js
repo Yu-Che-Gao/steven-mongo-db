a2d207f14926073ed543455e43c14bc46a5d8039
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-31 11:16:48 +0800

    再次測試

--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,11 @@ router.get('/', (req, res) => {
     res.send('you have no right to access this page.')
 });
 
-router.ws('/echo', require('./ws-echo'));
+// router.ws('/echo', require('./ws-echo'));
+router.ws('/echo', (ws, req) => {
+    ws.on('message', (msg) => {
+        ws.send(msg)
+    });
+})
 
 module.exports = router;
