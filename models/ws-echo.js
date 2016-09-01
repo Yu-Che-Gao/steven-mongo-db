2b4b404fa9cf1b61004d2f45e3482a054d5a146e
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-01 12:38:14 +0800

    json轉為string

--- models/ws-echo.js
+++ models/ws-echo.js
@@ -12,7 +12,7 @@ function echoDiary(ws, req) {
             console.log(result);
             return result;
         }).then((result)=>{
-            return ws.send(result);
+            return ws.send(result.stringify());
         }).catch((error)=>{
             console.log('error: '+error);
         });
